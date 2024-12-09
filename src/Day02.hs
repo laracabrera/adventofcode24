6442a95b8c0179b3e1b9de58e2c2131b27dd24a1
laracabrera <lara.cabrera@gmail.com>
2024-12-09 18:02:23 +0100

    Update

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,10 +1,17 @@
 module Day02 where
 
 import Paths_aoc2024 (getDataFileName)
+import Data.List.Split
+
+evaluateLine :: [Int] -> Bool
+evaluateLine l = all (\i -> i >= -3 && i <= -1) l || all (\i -> i >= 1 && i <= 3) l
 
 day02 :: IO ()
 day02 = do
   inputLines <- lines <$> (getDataFileName "day02-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 02"
+  let inputNums = map (splitOn " ") inputLines                -- inputNums = [["1", "4", "3", ...], ["2", "7"]]
+      inputInts = map (map (\x -> read x :: Int)) inputNums   -- inputInts = [[1, 4, 3, ...], [2, 7]]
+      diffs = map (\x -> zipWith (-) (tail x) x) inputInts
+      valids = map evaluateLine diffs
+      total = length (filter id valids)
+  print total
