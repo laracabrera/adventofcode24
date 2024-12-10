c84eeb0e96e47395fd5ee469f76829e54d3395be
laracabrera <lara.cabrera@gmail.com>
2024-12-10 12:35:57 +0100

    Solved Day 02

--- src/Day02.hs
+++ src/Day02.hs
@@ -4,14 +4,20 @@ import Paths_aoc2024 (getDataFileName)
 import Data.List.Split
 
 evaluateLine :: [Int] -> Bool
-evaluateLine l = all (\i -> i >= -3 && i <= -1) l || all (\i -> i >= 1 && i <= 3) l
+evaluateLine l = all (\i -> i >= -3 && i <= -1) ls || all (\i -> i >= 1 && i <= 3) ls
+                 where ls = zipWith (-) (tail l) l
+
+removingOne :: [Int] -> [[Int]]
+removingOne xs = [take i xs ++ drop (i+1) xs | i <- [0..length xs-1]]
+
+evaluateBoth :: [Int] -> Bool
+evaluateBoth xs = evaluateLine xs || any evaluateLine (removingOne xs)
 
 day02 :: IO ()
 day02 = do
   inputLines <- lines <$> (getDataFileName "day02-input.txt" >>= readFile)
   let inputNums = map (splitOn " ") inputLines                -- inputNums = [["1", "4", "3", ...], ["2", "7"]]
       inputInts = map (map (\x -> read x :: Int)) inputNums   -- inputInts = [[1, 4, 3, ...], [2, 7]]
-      diffs = map (\x -> zipWith (-) (tail x) x) inputInts
-      valids = map evaluateLine diffs
+      valids = map evaluateBoth inputInts
       total = length (filter id valids)
   print total
