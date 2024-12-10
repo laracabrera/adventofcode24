6bc7855f7ce9b1fbfd0042a4f7343f32692b62ff
laracabrera <lara.cabrera@gmail.com>
2024-12-10 19:14:26 +0100

    Update

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,10 +1,19 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 module Day03 where
 
 import Paths_aoc2024 (getDataFileName)
+import Text.Regex.PCRE ((=~))
+
+-- Part 1 regexp: mul\((\d{1,3}),(\d{1,3})\)
+
+pattern :: String
+pattern = "mul\\((\\d{1,3}),(\\d{1,3})\\)"
 
 day03 :: IO ()
 day03 = do
   inputLines <- lines <$> (getDataFileName "day03-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 03"
+  let matches = head inputLines =~ pattern :: [[String]]                               -- matches = [["mul(2,4)","2","4"],["mul(5,5)","5","5"],["mul(11,8)","11","8"],["mul(8,5)","8","5"]]
+      total = sum (map (\(_:a:b:_) -> (read a :: Int) * (read b :: Int)) matches)
+  print total
+
