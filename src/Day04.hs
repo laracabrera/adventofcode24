7debe4aa213bcf7d4ac64cf6fe8994151fb75eae
laracabrera <lara.cabrera@gmail.com>
2024-12-16 14:21:30 +0100

    Starting day04

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,9 +2,18 @@ module Day04 where
 
 import Paths_aoc2024 (getDataFileName)
 
+import Text.Regex.PCRE ((=~))
+import Data.Matrix
+
+pattern :: String
+pattern = "(XMAS|SAMX)"
+
+findInLines :: [String] -> Int
+findInLines = sum . map (=~ pattern)
+
 day04 :: IO ()
 day04 = do
   inputLines <- lines <$> (getDataFileName "day04-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 04"
+  let m = fromLists inputLines
+      columns = map (toList . (`getCol` m)) [0..ncols m-1]
+  print $ findInLines inputLines + findInLines columns + findInLines diags
